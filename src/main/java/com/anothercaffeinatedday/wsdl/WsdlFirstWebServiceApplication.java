9d2be7b429655a128223d5a23b142e0fdc2bf891
Timothy Stone <github@petmystone.com>
2020-11-01 13:46:57 -0500

    Commit initial project from Spring Initializr
    Initial project commit.

---
+++ src/main/java/com/anothercaffeinatedday/wsdl/WsdlFirstWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anothercaffeinatedday.wsdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlFirstWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsdlFirstWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anothercaffeinatedday/wsdl/WsdlFirstWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anothercaffeinatedday.wsdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlFirstWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
